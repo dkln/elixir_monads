9a79cb2af4562fd80bc075c7f400a91c23ed3ba1
Diederick Lawson <webmaster@altovista.nl>
2023-06-21 14:46:56 +0200

    Adds context option to map.()

--- lib/result.ex
+++ lib/result.ex
@@ -3,23 +3,38 @@ defmodule Monads.Result do
   defguard is_error(value) when is_tuple(value) and elem(value, 0) == :error
   defguard is_result(value) when is_ok(value) or is_error(value)
 
-  @type t :: {:ok, term} | {:error, term}
+  @type t :: {:ok, term} | {:error, term} | {:error, atom, term}
 
   @spec ok(term) :: t
   def ok(value) when is_function(value), do: ok(value.())
   def ok(value) when is_result(value), do: value
   def ok(value), do: {:ok, value}
 
+  @spec ok(term, atom) :: t
+  def ok(value, context) when is_function(value) and is_atom(context), do: ok(value.(), context)
+  def ok(value, context) when is_ok(value) and is_atom(context), do: value
+  def ok(value, context) when is_error(value) and is_atom(context), do: error(value, context)
+  def ok(value, context) when is_atom(context), do: value
+
   @spec error(term) :: t
   def error(value) when is_function(value), do: error(value.())
   def error(value) when is_result(value), do: value
   def error(value), do: {:error, value}
 
-  @spec new(term) :: t
-  def new(value) when is_result(value), do: value
-  def new(value) when is_function(value), do: new(value.())
-  def new(value), do: ok(value)
+  @spec error(term, atom) :: t
+  def error(value, context) when is_function(value) and is_atom(context), do: error(value.(), context)
 
+  def error(value, context) when is_result(value) and is_atom(context) do
+    if tuple_size(value) == 2 do
+      {:error, tuple_value(value), context}
+    else
+      value
+    end
+  end
+
+  def error(value, context), do: {:error, value, context}
+
+  @spec map(t, function | term) :: t
   def map(value, func) when is_ok(value) and is_function(func) do
     value
     |> tuple_value()
@@ -27,9 +42,19 @@ defmodule Monads.Result do
     |> ok()
   end
 
-  def map(value, new_value) when is_ok(value), do: ok(new_value)
-
   def map(value, _func) when is_error(value), do: error(value)
+
+  def map(_value, new_value), do: ok(new_value)
+
+  @spec map(t, atom, function | term) :: t
+  def map(value, context, _func) when is_error(value), do: error(value, context)
+
+  def map(value, context, func) when is_atom(context) and is_function(func) do
+    value
+    |> tuple_value()
+    |> func.()
+    |> ok(context)
+  end
 
   @spec tuple_value(tuple | term) :: term
   def tuple_value(value) when is_tuple(value),
--- test/result_test.exs
+++ test/result_test.exs
@@ -15,10 +15,18 @@ defmodule Monads.ResultTest do
     assert Monads.Result.error(fn -> {:error, :not_found} end) == {:error, :not_found}
   end
 
+  test "error/2" do
+    assert Monads.Result.error(:not_found, :get_user) == {:error, :not_found, :get_user}
+    assert Monads.Result.error({:error, :not_found}, :get_user) == {:error, :not_found, :get_user}
+    assert Monads.Result.error(fn -> :not_found end, :get_user) == {:error, :not_found, :get_user}
+    assert Monads.Result.error(fn -> {:error, :not_found} end, :get_user) == {:error, :not_found, :get_user}
+    assert {:error, :not_found, :get_user} |> Monads.Result.error(:save_user) == {:error, :not_found, :get_user}
+  end
+
   describe "map/2" do
     test "everything is ok" do
       assert "user"
-             |> Monads.Result.new()
+             |> Monads.Result.ok()
              |> Monads.Result.map(fn r ->
                assert r == "user"
                {:ok, 1}
@@ -41,7 +49,7 @@ defmodule Monads.ResultTest do
 
     test "something goes wrong" do
       assert "user"
-             |> Monads.Result.new()
+             |> Monads.Result.ok()
              |> Monads.Result.map(fn r ->
                assert r == "user"
                {:ok, 1}
@@ -51,6 +59,45 @@ defmodule Monads.ResultTest do
                {:error, :not_found}
              end)
              |> Monads.Result.map(fn _r -> :should_not_happen end) == {:error, :not_found}
+    end
+  end
+
+  describe "map/3" do
+    test "everything is ok" do
+      assert "user"
+             |> Monads.Result.ok()
+             |> Monads.Result.map(:get_user, fn r ->
+               assert r == "user"
+               {:ok, 1}
+             end)
+             |> Monads.Result.map(:get_profile, fn r ->
+               assert r == 1
+               2
+             end)
+             |> Monads.Result.map(:notify_user, fn r ->
+               assert r == 2
+               {:ok, 1, 2, 3}
+             end)
+             |> Monads.Result.map(:save_user, fn r ->
+               assert r == {1, 2, 3}
+               r
+             end)
+             |> Monads.Result.map(:saved) ==
+               {:ok, :saved}
+    end
+
+    test "something goes wrong" do
+      assert "user"
+             |> Monads.Result.ok()
+             |> Monads.Result.map(:get_user, fn r ->
+               assert r == "user"
+               {:ok, 1}
+             end)
+             |> Monads.Result.map(:get_profile, fn r ->
+               assert r == 1
+               {:error, :not_found}
+             end)
+             |> Monads.Result.map(:notify_user, fn _r -> :should_not_happen end) == {:error, :not_found, :get_profile}
     end
   end
 end
